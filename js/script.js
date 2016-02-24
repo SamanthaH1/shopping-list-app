2b7ca3a0c440255e53ce78c973be3046cbe12c78
SamanthaH1 <wisconsinflower2003@yahoo.com>
2016-02-24 15:41:08 -0600

    final commit

--- js/script.js
+++ js/script.js
@@ -1,41 +1,31 @@
+//javascript for shopping app.
 //check items off list
-$(document).ready(function(){
-//console.log($('#listitems'));
-	$('#listitems').on('click', 'li', function() {
-//		console.log(this);
-		$(this).toggleClass('crossout'); 
-		$(this).children('.box').toggleClass('box_chk');
-});
+$(document).ready(function() {
+    //console.log($('#listitems'));
+    $('#listitems').on('click', 'li', function() {
+        //		console.log(this);
+        $(this).toggleClass('crossout');
+        $(this).children('.box').toggleClass('box_chk');
+    });
+    $('.plus_icon').click(function() {
+   		console.log($('#item'))
+     	$('#listitems').append('<li> <div class="box"></div> <p>'+$('#item').val()+'</p> <img class="delete" src="images/delete.png"> </li>')
+    });
+
+    $('#listitems').sortable({
+		axis: "y"
+	});
+	$('#listitems').on('mouseenter', 'li', function(){
+		$(this).children('.delete').toggleClass('show');
+	});
+
+	$('#listitems').on('mouseleave', 'li', function(){
+		$(this).children('.delete').toggleClass('show');
+	});
+//delete list items
+	$('#listitems').on('click', '.delete', function(e){
+		e.preventDefault(); $(this).parent().remove()
 	});
 
 
-//add list items
-$('.plus_icon').click(function() {
-	$('.listitems').append($('<li>', {
-		text:$('#item').val()
-	}));
 });
-
-// ^^^^   codepen link:  http://codepen.io/SamanthaH1/pen/PZvzOa
-
-
-//sortable list items
-// sortable API http://api.jqueryui.com/sortable/#option-items
-
-	$('#listitems').sortable( 
-		{
-			axis: "y"
-		});
-
-
-//	show delete button on mouse hover ISNT WORKING
-
-//	$('#listitems').on('mouseenter', 'li', function(){
-//		$(this).children('.delete').toggleClass('show');
-//	});
-
-//	$('#listitems').on('mouseleave', 'li', function(){
-//		$(this).children('.delete').toggleClass('show');
-//	});
-
-// });

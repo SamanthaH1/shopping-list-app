8a9202c76751708983837f4f211c0f4521f4c372
SamanthaH1 <wisconsinflower2003@yahoo.com>
2016-02-24 15:15:50 -0600

    final changes

--- js/script.js
+++ js/script.js
@@ -1,45 +1,33 @@
 //javascript for shopping app.
-
 //check items off list
-$(document).ready(function(){
-//console.log($('#listitems'));
-	$('#listitems').on('click', 'li', function() {
-//		console.log(this);
-		$(this).toggleClass('crossout'); 
-		$(this).children('.box').toggleClass('box_chk');
-});
+$(document).ready(function() {
+    //console.log($('#listitems'));
+    $('#listitems').on('click', 'li', function() {
+        //		console.log(this);
+        $(this).toggleClass('crossout');
+        $(this).children('.box').toggleClass('box_chk');
+    });
+    $('.plus_icon').click(function() {
+   		console.log($('#item'))
+     	$('#listitems').append('<li> <div class="box"></div> <p>'+$('#item').val()+'</p> <img class="delete" src="images/delete.png"> </li>')
+    });
+
+    $('#listitems').sortable({
+		axis: "y"
 	});
-
-
-//add list items
-//not working
-// my codepen http://codepen.io/SamanthaH1/pen/PZvzOa
-
-$('.plus_icon').click(function() {
-	$('.listitems').append($('<li>', {
-		text:$('#item').val()
-	}));
-});
-
-
-//sortable list items
-//not working
-	$('#listitems').sortable( 
-		{
-			axis: "y"
-		});
-
-
-	//show delete button on mouse hover
-	//not working
-
 	$('#listitems').on('mouseenter', 'li', function(){
 		$(this).children('.delete').toggleClass('show');
 	});
-	
+
 	$('#listitems').on('mouseleave', 'li', function(){
 		$(this).children('.delete').toggleClass('show');
 	});
+//delete list items
+	$('#listitems').on('click', '.delete', function(e){
+		e.preventDefault(); $(this).parent().remove()
+	});
 
+
+});
 
 
